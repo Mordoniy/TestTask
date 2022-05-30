4a6c8e3760a9afba2546a3163691a7bd0b5d717e
Mikhail <Misha-zhu@mail.ru>
2022-05-30 13:10:45 +0700

    Добавлен рассчет урона

--- Assets/Scripts/BattleCalculate.cs
+++ Assets/Scripts/BattleCalculate.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class BattleCalculate 
+public static class BattleCalculate
 {
-
-
     public static float GetDamageAttack(CharacterParameters attacking, CharacterParameters target)
     {
-        
+        float koef = 1;
+        if (attacking.attack != 0 && target.defence != 0)
+            koef = (float) attacking.attack / target.defence;
+        else
+        {
+            if (attacking.attack == 0)
+                koef = 0;
+            if (target.defence == 0)
+                koef = 3;
+        }
 
+        if (attacking.attack == target.defence)
+            koef = 1;
 
-        return attacking.damage;
+        koef = Mathf.Clamp(koef, 1f / 3, 3);
+
+        return attacking.damage * koef;
     }
 }
